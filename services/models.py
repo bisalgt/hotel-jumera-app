2ef155716d5bc37a05f37db705d8c7650daf1b36
bisalgt <bisalgt@email.com>
2018-10-31 21:43:31 +0545

    commit for image responsive

--- services/models.py
+++ services/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Service(models.Model):
     image = models.ImageField(upload_to='services_pics')
+    title = models.CharField(max_length=50, default='None Heading/none')
     description = models.CharField(max_length=300)
-    
